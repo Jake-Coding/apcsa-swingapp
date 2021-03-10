6c96fccafd7541e1fe7b0d65d4405a041af1b5e9
Jake Bridge <jakebridge2005@gmail.com>
2021-03-10 11:12:24 -0700

    ListItem class implemented and basic functionality tested

---
+++ SwingApp/src/ItemTester.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+public class ItemTester {
+    JFrame f;
+
+    public ItemTester() throws Exception{
+        f = new JFrame("Item testing");
+
+        ListItem item = new ListItem("Test", "descTest", true);
+        System.out.println(item);
+        JComponent c = item.toComponent();
+        f.add(c);
+        f.setSize(500, 500);
+        f.setVisible(true);
+        Timer t = new Timer(3000, (event)-> {
+            System.out.println("slept");
+            f.remove(c);
+            item.toggleDone();
+            item.setDescription(" a new description! ");
+            
+            JComponent d = item.toComponent();
+            f.add(d);
+            f.setVisible(false);
+            f.setVisible(true);
+            System.out.println("pls");
+        });
+        t.setRepeats(false);
+        t.start();
+
+        
+
+    }
+   public static void main(String[] args) throws Exception {
+       new ItemTester();
+   } 
+}
---
+++ SwingApp/src/ListItem.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ListItem implements Comparable<ListItem>{
+    private String name;
+    private String description;
+    private boolean isDone;
+
+    public ListItem(String name, String description, boolean isDone) {
+        this.name = name;
+        this.description = description;
+        this.isDone = isDone;
+    }
+    public ListItem(String name, String description) {
+        this(name, description, false);
+    }
+    public ListItem(String name) {
+        this(name, "", false);
+    }
+    public ListItem() {
+        this("", "", false);
+    }
+
+    public String getName() {return name;}
+    public String getDescription() {return description;}
+    public boolean getIsDone() {return isDone;}
+    public boolean finished() {return getIsDone();}
+
+    public void setName(String name) {this.name = name;}
+    public void setDescription(String description) {this.description = description;}
+    public void setIsDone(boolean isDone) {this.isDone = isDone;}
+
+    public void toggleDone() {isDone = !isDone;}
+
+    public int compareTo(ListItem other) {
+        return this.name.compareTo(other.getName());
+    }
+
+    public String toString() {
+        return name + ":" + description + (isDone ? "finished" : "unfinished");
+    }
+
+    public JComponent toComponent() {
+        JComponent comp = new JPanel();
+        JLabel nameLabel = new JLabel(name);
+        JLabel descLabel = new JLabel(description);
+        JCheckBox doneBox = new JCheckBox();
+        doneBox.setSelected(isDone);
+        comp.add(nameLabel);
+        comp.add(descLabel);
+        comp.add(doneBox);
+        // GroupLayout layout = new GroupLayout(comp);
+        // layout.setAutoCreateGaps(true);
+        // layout.setAutoCreateContainerGaps(true);
+        // System.out.println(comp);
+        
+        // comp.setLayout(layout);
+        // comp.setVisible(true);
+
+        // return new JLabel("hi");
+        return comp;
+    }
+}
